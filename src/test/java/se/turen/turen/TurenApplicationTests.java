c61d69cb565372023cd75ccca8a0842773c95a6c
Dennis Mulder <dmulder1987@gmail.com>
2017-06-04 16:05:30 +0200

    first commit

---
+++ src/test/java/se/turen/turen/TurenApplicationTests.java
@@ -0,0 +1,16 @@
+package se.turen.turen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TurenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
